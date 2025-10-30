9d875543262cf4d31bd051531b45e4699f5c08da
angelicadelatorre41-art <angelicadelatorre41@gmail.com>
2025-10-30 22:24:18 +0800

    Running Time of Algorithms.cs

---
+++ Running Time of Algorithms.cs
@@ -0,0 +1,66 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'runningTime' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int runningTime(List<int> arr)
+    {
+         int shifts = 0;
+
+        for (int i = 1; i < arr.Count; i++)
+        {
+            int value = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > value)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+                shifts++; 
+            }
+
+            arr[j + 1] = value;
+        }
+
+        return shifts;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.runningTime(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
